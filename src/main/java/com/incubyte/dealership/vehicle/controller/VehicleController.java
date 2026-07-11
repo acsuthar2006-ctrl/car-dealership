bdec4df3686f4fd8e4e123c0bedd223d8228e3a7
Aarya Suthar <acsuthar2006@gmail.com>
2026-07-11 12:06:38 +0530

    GREEN: add validation for negative price

--- src/main/java/com/incubyte/dealership/vehicle/controller/VehicleController.java
+++ src/main/java/com/incubyte/dealership/vehicle/controller/VehicleController.java
@@ -3,6 +3,7 @@ package com.incubyte.dealership.vehicle.controller;
 import com.incubyte.dealership.vehicle.dto.VehicleRequest;
 import com.incubyte.dealership.vehicle.dto.VehicleResponse;
 import com.incubyte.dealership.vehicle.service.VehicleService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,7 +17,7 @@ public class VehicleController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public VehicleResponse addVehicle(@RequestBody VehicleRequest request) {
+    public VehicleResponse addVehicle(@Valid @RequestBody VehicleRequest request) {
         return vehicleService.addVehicle(request);
     }
 }
--- src/main/java/com/incubyte/dealership/vehicle/dto/VehicleRequest.java
+++ src/main/java/com/incubyte/dealership/vehicle/dto/VehicleRequest.java
@@ -1,9 +1,11 @@
 package com.incubyte.dealership.vehicle.dto;
 
+import jakarta.validation.constraints.Positive;
+
 public record VehicleRequest(
-        String make,
-        String model,
-        String category,
-        double price,
-        int quantityInStock) {
+                String make,
+                String model,
+                String category,
+                @Positive double price,
+                int quantityInStock) {
 }
