418797018006b879d582f9d2af726b269e6d5632
Aarya Suthar <acsuthar2006@gmail.com>
2026-07-11 12:13:56 +0530

    GREEN: add validation for blank strings in vehicle request

--- src/main/java/com/incubyte/dealership/vehicle/dto/VehicleRequest.java
+++ src/main/java/com/incubyte/dealership/vehicle/dto/VehicleRequest.java
@@ -1,11 +1,12 @@
 package com.incubyte.dealership.vehicle.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 
 public record VehicleRequest(
-                String make,
-                String model,
-                String category,
+               @NotBlank String make,
+               @NotBlank String model,
+               @NotBlank String category,
                 @Positive double price,
                 int quantityInStock) {
 }
